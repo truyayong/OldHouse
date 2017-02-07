15acb4fca8f2e1af10a9d0b24a5b7f343ed7f837
qiuyayong <804612066@qq.com>
2017-02-07 23:43:02 +0800

    User

--- app/src/main/java/com/truyayong/oldhouse/user/RelativeLayoutBehavior.java
+++ app/src/main/java/com/truyayong/oldhouse/user/RelativeLayoutBehavior.java
@@ -1,10 +1,15 @@
 package com.truyayong.oldhouse.user;
 
 import android.content.Context;
+import android.os.Handler;
+import android.os.Message;
 import android.support.design.widget.CoordinatorLayout;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 import android.widget.RelativeLayout;
+
+import com.truyayong.oldhouse.utils.DensityUtil;
 
 /**
  * Created by alley_qiu on 2017/2/7.
@@ -12,12 +17,30 @@ import android.widget.RelativeLayout;
 
 public class RelativeLayoutBehavior extends CoordinatorLayout.Behavior<RelativeLayout> {
 
+    Context mContext;
+
     public RelativeLayoutBehavior(Context context, AttributeSet attrs) {
         super(context, attrs);
+        mContext = context;
     }
 
     @Override
     public boolean layoutDependsOn(CoordinatorLayout parent, RelativeLayout child, View dependency) {
+
+        float max = DensityUtil.dp2px(mContext, 100) + DensityUtil.getStatusBarHeight(mContext);
+
+        Message msg = Message.obtain();
+
+        Log.e("QIU", " first getY : " + dependency.getY());
+        if (dependency.getY() > 0) {
+            float scale = ((dependency.getY() - max) / (DensityUtil.dp2px(mContext, 180) - max));
+            Log.e("QIU", "scale: " + scale + " max : " + max + "getY : " + dependency.getY());
+            child.setAlpha(scale);
+//            msg.what = (int) (scale * 100f);
+//            if (UserInfoActivity.mHandler != null) {
+//                UserInfoActivity.mHandler.sendMessage(msg);
+//            }
+        }
         return super.layoutDependsOn(parent, child, dependency);
     }
 
--- app/src/main/java/com/truyayong/oldhouse/user/UserInfoActivity.java
+++ app/src/main/java/com/truyayong/oldhouse/user/UserInfoActivity.java
@@ -1,30 +1,50 @@
 package com.truyayong.oldhouse.user;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.RelativeLayout;
 
 import com.truyayong.oldhouse.R;
 
 public class UserInfoActivity extends AppCompatActivity {
+
+    public static Handler mHandler;
+    private RelativeLayout rlHead;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_info);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("hello");
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+       // getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        rlHead = (RelativeLayout)findViewById(R.id.rl_head_userinfo);
+        mHandler = new Handler() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void handleMessage(Message msg) {
+                super.handleMessage(msg);
+                float alpha = ((float) msg.what) / 100f;
+                if (alpha < 0.5f) {
+                    rlHead.setAlpha(alpha * 0.5f);
+                } else {
+                    rlHead.setAlpha(alpha * 0.5f);
+                }
             }
-        });
+        };
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mHandler = null;
     }
 }
